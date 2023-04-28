38d15f4bf7bd5894d8081f5368b9b3c8de9099d5
dipa96 <d.dipasquale1@studenti.unisa.it>
2023-04-28 22:52:33 +0200

    update use cases

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # semgrep-rules
+
+## TODO
+
+[HackTricks DOM XSS Sinks & Sources](https://book.hacktricks.xyz/pentesting-web/xss-cross-site-scripting/dom-xss)
+Testing other challenges
+
+[AppSecco Blog Automating Discovery XSS](https://blog.appsecco.com/automating-discovery-and-exploiting-dom-client-xss-vulnerabilities-using-sboxr-part-3-2ea910dfb429)
+[DOMGO Challenge](https://domgo.at/cxss/intro)
--- vulnerable_snippets/js/use_cases.js
+++ vulnerable_snippets/js/use_cases.js
@@ -90,5 +90,52 @@ function trackSearch(query) {
       if (post) post.get(0).scrollIntoView();
   });
 
-  
+  // https://portswigger.net/web-security/dom-based/controlling-the-web-message-source/lab-dom-xss-using-web-messages-and-json-parse
+  window.addEventListener('message', function(e) {
+    var iframe = document.createElement('iframe'), ACMEplayer = {element: iframe}, d;
+    document.body.appendChild(iframe);
+    try {
+        d = JSON.parse(e.data);
+    } catch(e) {
+        return;
+    }
+    switch(d.type) {
+        case "page-load":
+            ACMEplayer.element.scrollIntoView();
+            break;
+        case "load-channel":
+            ACMEplayer.element.src = d.url;
+            break;
+        case "player-height-changed":
+            ACMEplayer.element.style.width = d.width + "px";
+            ACMEplayer.element.style.height = d.height + "px";
+            break;
+    }
+}, false);
 
+
+// http://www.domxss.com/domxss/01_Basics/00_simple_noHead.html
+var pos=document.URL.indexOf("name=")+5;
+var username = unescape(document.URL.substring(pos,document.URL.length));
+var r='<b>'+username+'</b>'
+document.write(r);
+
+// http://www.domxss.com/domxss/01_Basics/04_eval.htm
+function loadObj(){
+    var cc=eval('('+unescape(aMess)+')');
+    document.getElementById('mess').textContent=cc.message;
+   }
+   
+   if(window.location.hash.indexOf('message')==-1)
+     var aMess="({\"message\":\"Hello User!\"})";
+   else
+     var aMess=location.hash.substr(window.location.hash.indexOf('message=')+8);
+
+// http://www.domxss.com/domxss/01_Basics/05_jquery_html.html
+// https://security.stackexchange.com/questions/47638/how-is-dom-xss-possible-here
+function setMessage(){
+    var t=unescape(location.hash.slice(1));
+    $("div[id="+t+"]").html("Message from the name "+window.name);
+    }
+    $(document).ready(setMessage  );
+    $(window).bind("hashchange",setMessage)
